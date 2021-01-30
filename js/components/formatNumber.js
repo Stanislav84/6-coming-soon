885f4070b0b85f39b4c5abfa62ed1d5607181d88
Stanislav84 <76004794+Stanislav84@users.noreply.github.com>
2021-01-30 21:57:14 +0200

    Validation functions for "render progress bars" #20

---
+++ js/components/formatNumber.js
@@ -0,0 +1,28 @@
+function formatNumber(number, positions = 2) {
+ if (typeof number !== 'number' ||
+!isFinite(number)) {
+    console.error('ERROR: pirmasis parametras turi buti tikras skaicius.');
+    return false;
+}
+if (typeof positions !== 'number' ||
+!isFinite(positions) ||
+     positions % 1 !== 0 ||
+     positions < 0 ||
+     position > 10) {
+    console.error('ERROR: antrasis parametras turi buti tikras skaicius.');
+    return false;
+ }
+
+
+    const fixed = 10** positions;
+   return Math.round(number * fixed) / fixed;
+}
+
+export { formatNumber }
+
+
+
+
+
+
+
